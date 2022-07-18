237109ed50238656259bcf3d0dff879e3c04a1fc
V14dik <cherenkevich.vlad@gmail.com>
2022-07-18 22:05:16 +0300

    Add start page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
 import Registration from "./component/Registration";
+import { HelloWorld } from "./component/HelloWorld";
 
 class App extends Component {
   render() {
     return (
       <Routes>
         <Route path="/registration" element={<Registration />} />
+        <Route path="/" element={<HelloWorld />} />
       </Routes>
     );
   }
---
+++ src/component/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World </h1>
+      <Link to={"/registration"}>Registration</Link>
+    </div>
+  );
+};
